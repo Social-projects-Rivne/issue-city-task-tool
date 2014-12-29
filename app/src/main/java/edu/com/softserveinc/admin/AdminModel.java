59b2ad81956ab9a9630f4e44c8becf62f9b10134
Nazar Kostecki <silver_925@ukr.net>
2014-12-29 16:28:48 +0200

    add validation for new users (I have changed home.jsp (it don works))

---
+++ app/src/main/java/edu/com/softserveinc/admin/AdminModel.java
@@ -0,0 +1,23 @@
+package edu.com.softserveinc.admin;
+ import edu.com.softserveinc.user.UserModel;
+
+/**
+ * 
+ * Admin model class
+ * 
+ * @author nazar
+ *
+ */
+public class AdminModel extends UserModel{
+	
+	public AdminModel(String name, String email, String login, int role_id,
+			String password, String avatar) {
+		super(name, email, login, role_id, password, avatar);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+    
+    
+}
