27900e2a74d60055b05961e7c9535734872ccecf
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-01-05 12:12:47 +0200

    Bind add-user-button

--- app/src/main/java/edu/com/softserveinc/AdminToolpageController.java
+++ app/src/main/java/edu/com/softserveinc/AdminToolpageController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import edu.com.softserveinc.main.dao.AddUserImpl;
 import edu.com.softserveinc.main.models.UserModel;
+
 import java.util.List; 
- 
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.hibernate.HibernateException; 
 import org.hibernate.Session; 
 import org.hibernate.Transaction;
