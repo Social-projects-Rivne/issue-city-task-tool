475a77cf6cef7eda56037fe33c6e91027df93bb9
zorin <mykhailo.zorin@gmail.com>
2015-01-12 17:45:41 +0200

    Modify home.jsp, HomeController and styles2.css

--- app/src/main/java/edu/com/softserveinc/HomeController.java
+++ app/src/main/java/edu/com/softserveinc/HomeController.java
@@ -1,7 +1,5 @@
 package edu.com.softserveinc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		return "home";
