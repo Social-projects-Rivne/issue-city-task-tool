c4da1643eb0ff39f908e86b49755ca5332deecc1
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-01-13 15:25:05 +0200

    Add home-script.js, bind event to show dialog window

---
+++ app/src/main/webapp/resources/js/home-script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	initialize();
+	validate();
+	
+	var map = document.getElementById('map');
+	var issue = document.getElementById('add-issue-form');
+	
+	map.addEventListener('click', function() {
+		issue.style.display = 'block';
+	}, false);
+}
