7f774f596943114830b85d5c83a36e3cd0579b36
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-01-22 12:21:18 +0200

    Created CRUD operations for comment model

---
+++ app/src/main/java/edu/com/softserveinc/main/services/CommentServiceImpl.java
@@ -0,0 +1,39 @@
+package edu.com.softserveinc.main.services;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.com.softserveinc.main.dao.DaoImpl;
+import edu.com.softserveinc.main.interfaces.CommentService;
+import edu.com.softserveinc.main.models.CommentModel;
+
+public class CommentServiceImpl implements CommentService {
+	@Autowired
+	SessionFactory sessionFactory;
+
+	@Override
+	public void addComment(CommentModel comment) {
+		new DaoImpl().addInDB(comment);
+	}
+
+	@Override
+	public void deleteComment(CommentModel comment) {
+		new DaoImpl().deleteFromDB(comment);
+	}
+
+	@Override
+	public void editComment(CommentModel comment) {
+		new DaoImpl().editInDB(comment);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List getCommentsByIssueId(int issueId) {
+		return sessionFactory.getCurrentSession().createQuery("FROM CommentModel WHERE issueId="
+			+ issueId).list();
+	}
+}
