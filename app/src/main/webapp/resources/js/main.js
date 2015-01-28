abbe476a8877a9c9761715fbd08be6161626b900
Nazar Kostecki <silver_925@ukr.net>
2015-01-28 15:25:58 +0200

    fix main js

--- app/src/main/webapp/resources/js/main.js
+++ app/src/main/webapp/resources/js/main.js
@@ -1,34 +1,49 @@
 function sendNewComment(){
 	
-	var comment = new CommentModel({'userName':'Nazar','comment':'comment bla ba l lva'});
-	var comm = "Comment bla bla";
-	//comment.set("comment", comm);
-
-	//comment.set("userName", "Nazar");
+	var comment = new CommentModel({
+		'userName':$(document.getElementsByName('userName')[0]).val(),
+		'comment':$(document.getElementsByName('comment-text')[0]).val(),
+		'email' : $(document.getElementsByName('email')[0]).val(),
+		'issueId' : ""
+		
+	});
+	
 	var commentView = new CommentViev({
 		model : comment
 	});
+	
 	commentView.render();
+	
 	$(document.body.getElementsByClassName('comments')[0]).append(commentView.el);
+	
 	console.log(comment.toJSON());
-	sendAjax();
+	
+	console.log(comment),
+	
+	sendAjax( '{"email":"' + document.getElementsByName("email")[0].value +
+			'","userName":"' + document.getElementsByName("userName")[0].value  +
+			'", "comment": "' + document.getElementsByName("comment-text")[0].value  + 
+			'", "issueId":"1"}');
 }
 
 
-function sendAjax() {
+function sendAjax(comment) {
 
 	$
 			.ajax({
 				url : "/Bawl/add-comment",
 				type : 'POST',
 				dataType : 'json',
-				data : '{"email":"' + document.getElementsByName("email")[0].value + '","userName":"' + document.getElementsByName("userName")[0].value  + '", "comment": "' + document.getElementsByName("comment-text")[0].value  + '", "issueId":"1"}',
+				//data : '{"email":"' + document.getElementsByName("email")[0].value + '","userName":"' + document.getElementsByName("userName")[0].value  + '", "comment": "' + document.getElementsByName("comment-text")[0].value  + '", "issueId":"1"}',
+				data: comment,
 				contentType : 'application/json',
 				mimeType : 'application/json',
 				success : function(data) {
 					alert(data.email + " " + data.issueId);
+					console.log(comment);
 				},
 				error : function(data, status, er) {
+					console.log(comment);	
 					alert("error: " + data + " status: " + status + " er:" + er);
 				}
 			});
