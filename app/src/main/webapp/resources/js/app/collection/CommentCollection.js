2a9789fc7811eea11c0e3324b6a79e3593d7e3b6
Nazar Kostecki <silver_925@ukr.net>
2015-01-29 15:26:29 +0200

    add url for fetching comments

--- app/src/main/webapp/resources/js/app/collection/CommentCollection.js
+++ app/src/main/webapp/resources/js/app/collection/CommentCollection.js
@@ -2,10 +2,10 @@ define([ 'underscore', 'backbone', 'model/CommentModel' ], function(_,
 		Backbone, CommentModel) {
 
 	var CommentCollection = Backbone.Collection.extend({
-		intialize : function() {
+		intialize : function(/* add here issue_id*/) {
 
 		},
-
+		url: 'all-comments/1', /* add here issue_id*/
 		model : CommentModel
 	});
 	return CommentCollection;
