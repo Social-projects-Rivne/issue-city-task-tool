b684d539e18ce249864446df6ab2d9a3690f3510
Nazar Kostecki <silver_925@ukr.net>
2015-01-31 01:39:47 +0200

    fix another bug after smoke test

--- app/src/main/java/edu/com/softserveinc/main/controllers/HomeController.java
+++ app/src/main/java/edu/com/softserveinc/main/controllers/HomeController.java
@@ -63,11 +63,18 @@ public class HomeController {
 
 		IssueModel issue = new IssueModel(category.getId(), issueName,
 				issueDescription, mapPointer, issueAttachments, 1);
+		
+		System.out.println("MAP POINTER +++++++++++  = " + mapPointer+ " | " + issueName+ " | " +
+				issueDescription+ " | " + mapPointer+ " | " + issueAttachments);
+		
+		//issueAttachments = "I've add this becouse...";
+		
 
+		service.addProblemm(issue);
+		
 		if (new IssueValidator(issue).isValid()) {
 			try {
-				service.addProblemm(issue);
-				System.out.println("MAP POINTER +++++++++++  = " + mapPointer);
+				
 			} catch (Exception ex) {
 				System.out.println("ERROR! Issue is not valid!!!!! "
 						+ ex.toString());
