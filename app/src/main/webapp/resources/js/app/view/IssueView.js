c2aef49b4a75319e632023012006d50f4f200369
NazarKo <silver_925@ukr.net>
2015-02-03 16:53:01 +0200

    fix issueView.js

--- app/src/main/webapp/resources/js/app/view/IssueView.js
+++ app/src/main/webapp/resources/js/app/view/IssueView.js
@@ -1,37 +1,59 @@
-define([ 'underscore', 'backbone', 'text!templates/IssueDetails.html'], function(_,
-		Backbone, IssueDetailsTemplate) {
+define([ 'jquery', 'underscore', 'backbone',
+		'text!templates/IssueDetails.html', 'text!templates/AddIssue.html',
+		'model/IssueModel',],
+		function($, _, Backbone, IssueDetailsTemplate, AddIssueTemplate, IssueModel) {
 
-		var IssueView = Backbone.View.extend({		
-			
-			//model : IssueModel,
-			tagName : 'div',
-			className : 'grid',
-			template : _.template(IssueDetailsTemplate),
-		
-			events : {
-				'click #cry-out' : 'addIssueForm',
-			},
+			var IssueView = Backbone.View.extend({
 
+				model : IssueModel,
+				//className :'grid',
+				//el: '#issue-form',
 
-			intialize : function(issueId) {
+				IssueDetailsTemplate : _.template(IssueDetailsTemplate),
+				AddIssueTemplate : _.template(AddIssueTemplate),
 
-			},
+				events : {
+					'click #cry-out' : 'addIssueForm',
+					'click #myForestGump' : 'issueDetailsForm',
+					'click .leaflet-clickable' : 'issueDetailsForm',
+				},
 
-			addIssueForm : function(){
-				console.log('cry-out button event');
-				this.render();
-			},
-		
-		//TODO: render from collection is incorrect! Remove it later
-		render : function() {
-				console.log('Render function working');
-				this.$el.html(this.template); // add (this.model.toJSON()) in template when model will be done
-				console.log('el changed');
-				$('#add-issue').append(this.el);
-				console.log('apended');
-			}
+				/*addIssueForm : function(e){
+					console.log('cry-out button event');
+					this.render();
+				},*/
+
+				runForest : function(e) {
+					alert('I`m runninig! ');
+				},
+				// TODO: render from collection is incorrect! Remove it later
+				render : function() {
+					console.log('Render function working');
+
+				},
+				//this.model.toJSON())
+				issueDetailsForm : function() {
+					// clear block with form
+					$('.grid #issue-form').innerHTML = '';
+					var temp = this.$el;
+					this.$el = $("#issue-form");
+					this.$el.html(this.IssueDetailsTemplate);
+					this.$el = temp;
+					console.log('apended');
+				},
+
+				addIssueForm : function() {
+					// clear block with form
+					$('.grid #issue-form').innerHTML = '';
+					var temp = this.$el;
+					this.$el = $("#issue-form");
+					this.$el.html(this.AddIssueTemplate);
+					this.$el = temp;
+					console.log('apended');
+				}
+
+			});
+
+			return IssueView;
+
 		});
-
-	return IssueView;
-
-});
