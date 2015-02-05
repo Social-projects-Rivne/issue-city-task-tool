47d6100e80a7d5661f064fcb245d65b61c1eb9df
zorin <mykhailo.zorin@gmail.com>
2015-02-05 17:57:02 +0200

    Added MapView.js

---
+++ app/src/main/webapp/resources/js/app/view/MapView.js
@@ -0,0 +1,25 @@
+define([ 'jquery', 'underscore', 'backbone', 'leaflet', 'collection/IssueCollection' ],
+		function($, _, Backbone, L, IssueCollection) {
+			var MapView = Backbone.View.extend({
+				initialize : function() {
+					this.model = new IssueCollection();
+					this.model.fetch();
+				},
+				
+				render : function() {
+					var map = L.map('map').setView([50.62, 26.25], 13);
+					
+					L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+					    maxZoom: 18
+					}).addTo(map);
+					
+					var that = this;
+					this.model.each(function(issue) {
+						var tempMarker = L.marker(issue.get("mapPointer").substr(7, element.mapPointer.length - 1)
+								.split(', ')).addTo(map);
+					});
+				}
+			});	
+			
+			return MapView;
+		})
