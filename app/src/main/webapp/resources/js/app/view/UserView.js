8acee2e23a836efaa5c88434e7a4485682055f07
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-02-05 15:07:16 +0200

    Modified UserView

--- app/src/main/webapp/resources/js/app/view/UserView.js
+++ app/src/main/webapp/resources/js/app/view/UserView.js
@@ -1,12 +1,18 @@
 define([ 'jquery', 'underscore', 'backbone', 'model/UserModel' ],
 		function($, _, Backbone, UserModel) {
 			var UserView = Backbone.View.extend({
+				tagName : "tr",
+				
 				initialize : function() {
 					this.model = new UserModel();
 				},
 				
 				render : function() {
-					this.$el.html();
+					this.$el.html("<td>" + this.model.get(login) + "</td>" +
+							"<td>" + this.model.get(email) + "</td>" +
+							"<td>" + this.model.get(name) + "</td>");
+					
+					return this;
 				}
 			});	
 			
