4adbb1151926d9e0fe899e208fdbfd79fbb2f795
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-02-10 15:21:12 +0200

    Binded next-buttons and tabs on add-issue form

--- app/src/main/webapp/resources/js/app/view/AddIssueView.js
+++ app/src/main/webapp/resources/js/app/view/AddIssueView.js
@@ -8,19 +8,75 @@ define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel', 'text!templates
 				},
 				
 				events: {
-					'click': 'addIssue'
+					'click #next-to-description': 'nextToDescription',
+					'click #next-to-photo': 'nextToPhoto',
+					'click #tab1-title > a': 'tabChanger',
+					'click #tab2-title > a': 'tabChanger',
+					'click #tab3-title > a': 'tabChanger',
 				},
 				
 				render: function() {
 					this.$el.html(this.template);
 					
 					return this;
+				},
+				
+				nextToDescription: function(e) {
+					e.preventDefault();
+					this.$el.find('#tab1-title').removeClass('active');
+					this.$el.find('#tab2-title').addClass('active');
+					this.$el.find('#tab1').removeClass('active');
+					this.$el.find('#tab1').addClass('fade');
+					this.$el.find('#tab2').removeClass('fade');
+					this.$el.find('#tab2').addClass('active');
+				},
+				
+				nextToPhoto: function(e) {
+					e.preventDefault();
+					this.$el.find('#tab2-title').removeClass('active');
+					this.$el.find('#tab3-title').addClass('active');
+					this.$el.find('#tab2').removeClass('active');
+					this.$el.find('#tab2').addClass('fade');
+					this.$el.find('#tab3').removeClass('fade');
+					this.$el.find('#tab3').addClass('active');
+				},
+				
+				tabChanger: function(e) {
+					e.preventDefault();
+					
+					if(this.$el.find('#tab1-title').hasClass('active')) {
+						this.$el.find('#tab1-title').removeClass('active');
+						this.$el.find('#tab1').removeClass('active');
+						this.$el.find('#tab1').addClass('fade');
+					}
+					else if(this.$el.find('#tab2-title').hasClass('active')) {
+						this.$el.find('#tab2-title').removeClass('active');
+						this.$el.find('#tab2').removeClass('active');
+						this.$el.find('#tab2').addClass('fade');
+					}
+					else {
+						this.$el.find('#tab3-title').removeClass('active');
+						this.$el.find('#tab3').removeClass('active');
+						this.$el.find('#tab3').addClass('fade');
+					}
+					
+					if(e.currentTarget.parentNode.id == 'tab1-title') {
+						this.$el.find('#tab1-title').addClass('active');
+						this.$el.find('#tab1').removeClass('fade');
+						this.$el.find('#tab1').addClass('active');
+					}
+					else if(e.currentTarget.parentNode.id == 'tab2-title') {
+						this.$el.find('#tab2-title').addClass('active');
+						this.$el.find('#tab2').removeClass('fade');
+						this.$el.find('#tab2').addClass('active');
+					}
+					else {
+						this.$el.find('#tab3-title').addClass('active');
+						this.$el.find('#tab3').removeClass('fade');
+						this.$el.find('#tab3').addClass('active');
+					}
 				}
 			});
-			
-			function addIssue() {
-				
-			}
 			
 			return AddIssueView;
 		})
