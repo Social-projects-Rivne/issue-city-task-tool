955808db124c45741e692aae627b31485f91d1ad
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-02-11 12:52:24 +0200

    Added admin template to admin page

--- app/src/main/webapp/resources/js/app/view/UserListView.js
+++ app/src/main/webapp/resources/js/app/view/UserListView.js
@@ -1,16 +1,18 @@
 define([ 'jquery', 'underscore', 'backbone', 'model/UserModel', 
-        'collection/UserCollection', 'view/UserView' ],
-		function($, _, Backbone, UserModel, UserCollection, UserView) {
+        'collection/UserCollection', 'view/UserView', 'text!templates/Admin.html' ],
+		function($, _, Backbone, UserModel, UserCollection, UserView, AdminTemplate) {
 			var UserListView = Backbone.View.extend({
 				initialize : function() {
 					this.model = new UserCollection();
 				},
 				
+				template: _.template(AdminTemplate),
+				
 				render : function() {
 					var that = this;
 					
 					this.model.fetch( { success: function() {
-						that.$el.html("<table>");
+						that.$el.html(that.template);
 						that.model.each(function(user) {
 							var userView = new UserView( { model: user } );
 							that.$el.find("table").append(userView.render().$el);
