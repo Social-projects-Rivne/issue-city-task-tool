f0d71744f0e1ccbd8bd7960d37a9ad675c88cb9d
Yaroslav Gamuga <main@neutralpoint.org.ua>
2015-02-12 02:23:26 +0200

    Created backboneJS UserModel and  binded add-new-user button on AddUser template.

--- app/src/main/webapp/resources/js/app/model/UserModel.js
+++ app/src/main/webapp/resources/js/app/model/UserModel.js
@@ -1,7 +1,16 @@
 define([ 'underscore', 'backbone' ], function(_, Backbone) {
 
 	var UserModel = Backbone.Model.extend({
-		urlRoot: '',
+		urlRoot: 'admin/add-user',
+		id:'',
+		defaults: {
+			//id: 0,
+			name: "",
+			email: "",
+			login: "",
+			password: 0,
+			avatar:""
+		}
 	});
 	
 	return UserModel;
