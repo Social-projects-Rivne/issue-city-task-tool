e2022292af217ef34bb713c4cc5ecde2e6434f06
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-02-12 15:55:57 +0200

    Modified deleting users

--- app/src/main/webapp/resources/js/app/view/UserListView.js
+++ app/src/main/webapp/resources/js/app/view/UserListView.js
@@ -16,7 +16,9 @@ define([ 'jquery', 'underscore', 'backbone', 'model/UserModel',
 				},
 				
 				events: {
-					'click .glyphicon-remove': 'confirmation'
+					'click .glyphicon-remove': 'confirmation',
+					'click #confirm-remove': 'confirmRemove',
+					'click #reject-remove': 'rejectRemove'
 				},
 				
 				template: _.template(AdminTemplate),
@@ -25,15 +27,24 @@ define([ 'jquery', 'underscore', 'backbone', 'model/UserModel',
 					
 					removeUserConfirmationView = new RemoveUserConfirmationView( { el: "#container" } );
 					
-					that.$el.html(that.template);
-					that.model.each(function(user) {
-						var userView = new UserView( { model: user } );
-						that.$el.find("table").append(userView.render().$el);
-					});
+					this.model.fetch( { success: function() {
+						that.$el.html(that.template);
+						that.model.each(function(user) {
+							var userView = new UserView( { model: user } );
+							that.$el.find("table").append(userView.render().$el);
+						});
+					} } );
 				},
 				
 				confirmation: function(e) {
 					removeUserConfirmationView.render(e.currentTarget.parentNode.id);
+				},
+				
+				confirmRemove: function(e) {
+					var user = new UserModel();
+					user.set('id', e.currentTarget.name);
+					user.destroy( { url: 'remove-user/' + user.get('id') } );
+					that.render();
 				}
 			});	
 			
