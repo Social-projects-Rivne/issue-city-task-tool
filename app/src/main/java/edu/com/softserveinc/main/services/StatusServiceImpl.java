b77c5c7db52727ca8014d21cd091f59e11199338
zorin <mykhailo.zorin@gmail.com>
2015-02-16 18:17:14 +0200

    Modified StatusServiceImpl

--- app/src/main/java/edu/com/softserveinc/main/services/StatusServiceImpl.java
+++ app/src/main/java/edu/com/softserveinc/main/services/StatusServiceImpl.java
@@ -2,12 +2,14 @@ package edu.com.softserveinc.main.services;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import edu.com.softserveinc.main.dao.DaoImpl;
 import edu.com.softserveinc.main.interfaces.StatusService;
+import edu.com.softserveinc.main.models.CategoryModel;
 import edu.com.softserveinc.main.models.StatusModel;
 
 public class StatusServiceImpl implements StatusService {
@@ -25,8 +27,16 @@ public class StatusServiceImpl implements StatusService {
 				.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		Query query = session.createQuery("FROM StatusModel WHERE name = :name ");
+		query.setParameter("name", name);
+		StatusModel statusModel = null;
+		for(int i = 0; i < query.list().size(); i++) {
+			statusModel = (StatusModel)query.list().get(i);
+			if(statusModel.getName() == name)
+				break;
+		}
 		
-		return (StatusModel) session.createQuery("FROM StatusModel WHERE name =" + name);
+		return statusModel;
 	}
 	
 	@SuppressWarnings("rawtypes")
