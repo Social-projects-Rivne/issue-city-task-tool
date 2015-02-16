1541d41c7074f02eade3cd25e386edc09410d30d
zorin <mykhailo.zorin@gmail.com>
2015-02-16 18:19:43 +0200

    Modified StatusServiceImpl

--- app/src/main/java/edu/com/softserveinc/main/services/StatusServiceImpl.java
+++ app/src/main/java/edu/com/softserveinc/main/services/StatusServiceImpl.java
@@ -9,7 +9,6 @@ import org.hibernate.cfg.Configuration;
 
 import edu.com.softserveinc.main.dao.DaoImpl;
 import edu.com.softserveinc.main.interfaces.StatusService;
-import edu.com.softserveinc.main.models.CategoryModel;
 import edu.com.softserveinc.main.models.StatusModel;
 
 public class StatusServiceImpl implements StatusService {
