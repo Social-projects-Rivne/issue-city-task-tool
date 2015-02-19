559b46f4cf8cdc6739b018fbd5e46300c6b24497
Nazar Kostecki <silver_925@ukr.net>
2015-02-19 14:26:08 +0200

    add method for getting user by name

--- app/src/main/java/edu/com/softserveinc/main/dao/UserDao.java
+++ app/src/main/java/edu/com/softserveinc/main/dao/UserDao.java
@@ -1,7 +1,13 @@
 package edu.com.softserveinc.main.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import edu.com.softserveinc.main.models.UserModel;
  
 public interface UserDao extends JpaRepository<UserModel, Integer>{
+	@Query("select users from UserModel users where users.name = :name")
+	UserModel findByName(@Param("name") String name);
+	
 }
