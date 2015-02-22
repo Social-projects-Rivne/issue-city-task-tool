61711304fc162d6628c41b894138aeeae2ced43b
Roman Vasylyshyn <roman@vasylyshyn.name>
2015-02-22 16:06:47 +0200

    Removed @Query anotations in repositories, as queries already derives from the method names

--- app/src/main/java/edu/com/softserveinc/main/dao/CommentDao.java
+++ app/src/main/java/edu/com/softserveinc/main/dao/CommentDao.java
@@ -3,13 +3,10 @@ package edu.com.softserveinc.main.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import edu.com.softserveinc.main.models.CommentModel;
  
 public interface CommentDao extends JpaRepository<CommentModel, Integer>{
 	
-	@Query("select c from CommentModel c where c.issueId = :issueId")
-	List<CommentModel> findByIssueId(@Param("issueId") int issueId);
+	List<CommentModel> findByIssueId(int issueId);
 }
