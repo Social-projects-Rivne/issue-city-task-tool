686076946eb013f0472e1d50f3883821ea1df70b
NazarKo <silver_925@ukr.net>
2015-02-25 18:54:26 +0200

    add notification for commen and half of validation

--- app/src/main/webapp/resources/js/app/view/IssueDetailsView.js
+++ app/src/main/webapp/resources/js/app/view/IssueDetailsView.js
@@ -1,6 +1,8 @@
-define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel','model/CommentModel', 'view/CommentView', 'text!templates/IssueDetails.html',  ],
-		function($, _, Backbone, IssueModel, CommentModel, CommentView, IssueDetailsTemplate) {
+define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel','model/CommentModel', 'view/CommentView', 'text!templates/IssueDetails.html', 'text!templates/NotificationTemplate.html', ],
+		function($, _, Backbone, IssueModel, CommentModel, CommentView, IssueDetailsTemplate, NotificationTemplate) {
 			var IssueDetaisView = Backbone.View.extend({
+				
+				notificationTemplate: _.template(NotificationTemplate),
 				template: _.template(IssueDetailsTemplate),
 				
 				initialize: function() {
@@ -26,35 +28,43 @@ define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel','model/CommentMo
 				},
 				
 				addComment : function() {
-					
-				 comment = new CommentModel({
-					 "email": $(document.getElementsByName('email')[0]).val(),
-					 "userName": $(document.getElementsByName('userName')[0]).val(),
-					 "comment":  $(document.getElementsByName('comment-text')[0]).val(),
-					 "issueId":  this.model.id });
-				 try{
-					 comment.save();
-					 console.log('comment add function');
-					 
-					 
-					 var commentView = new CommentView({
-							model : comment
-					 });
-					 commentView.render();
-					 
-					 //clan fields
-					 alert(comment.get('userName') + ", thanks you for yours comment!" );
+				if(($('#add-comment  [name*="userName"]').val() !='') && ($('#add-comment  [name*="email"]').val() !='')){	
+					 comment = new CommentModel({
+						 "email": $(document.getElementsByName('email')[0]).val(),
+						 "userName": $(document.getElementsByName('userName')[0]).val(),
+						 "comment":  $(document.getElementsByName('comment-text')[0]).val(),
+						 "issueId":  this.model.id });
+					 try{
+						 comment.save();
+						 console.log('comment add function');
+						 
+						 
+						 var commentView = new CommentView({
+								model : comment
+						 });
+						 commentView.render();
+						//notification						
+						if($('#notificationModal')) $('#notificationModal').remove();
+						$('body').append(this.notificationTemplate( { 'data': { 'message': comment.get('userName') + ", thanks you for yours comment!" } } ));
+						$('#notificationModal').modal();
+						//clan fields
 						document.getElementsByName('email')[0].value = "";
 						document.getElementsByName('userName')[0].value = "";
 						document.getElementsByName('comment-text')[0].value = "";
-						
-				 }catch(error){
-					 alert(error);
-				 }
-				 
-				 
-				}
-				
+							
+					 }catch(error){
+						if($('#notificationModal')) $('#notificationModal').remove();
+						$('body').append(this.notificationTemplate( { 'data': { 'message': error } } ));
+						$('#notificationModal').modal();
+					 }
+					 
+					 
+					}else{
+						if($('#notificationModal')) $('#notificationModal').remove();
+						$('body').append(this.notificationTemplate( { 'data': { 'message': "Error! Input your name!" } } ));
+						$('#notificationModal').modal();
+				}	
+			}
 			});	
 			
 			return IssueDetaisView;
