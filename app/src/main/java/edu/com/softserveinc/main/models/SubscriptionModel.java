4432947e1e4a573207aff7f63a84c476b0044a17
Roman Vasylyshyn <roman@vasylyshyn.name>
2015-02-26 21:18:37 +0200

    Added SubscriptionModel

---
+++ app/src/main/java/edu/com/softserveinc/main/models/SubscriptionModel.java
@@ -0,0 +1,61 @@
+package edu.com.softserveinc.main.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+@Table(name = "subscriptions")
+public class SubscriptionModel {
+	
+	@Id
+	@GeneratedValue
+	@Column(unique=true, name = "id")
+	int id;
+
+	@NotNull
+	@Column(name = "issueId")
+	private int issueId;
+
+	@Email
+	@Column(name = "email")
+	private String email;
+
+	public SubscriptionModel() {
+	}
+
+	public SubscriptionModel(int issueId, String email) {
+		this.issueId = issueId;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIssueId() {
+		return issueId;
+	}
+
+	public void setIssueId(int issueId) {
+		this.issueId = issueId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
