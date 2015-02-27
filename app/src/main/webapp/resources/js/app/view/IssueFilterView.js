7046266ec9e81ad60722729bf6a98d72d568c7e1
Nazar Kostecki <silver_925@ukr.net>
2015-02-27 16:05:28 +0200

    add some methods in issue filter view

--- app/src/main/webapp/resources/js/app/view/IssueFilterView.js
+++ app/src/main/webapp/resources/js/app/view/IssueFilterView.js
@@ -1,20 +1,37 @@
-define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel', 'text!templates/issue_filter.html', ],
-		function($, _, Backbone, IssueModel, IssueFilterTemplate) {
+define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel', 'collection/CategoryCollection',  'text!templates/issue_filter.html', ],
+		function($, _, Backbone, IssueModel, CategoryCollection, IssueFilterTemplate) {
 			var IssueFilterView = Backbone.View.extend({
 				
-				template: IssueFilterTemplate,
+				categoryCollection: new CategoryCollection(),
+				template: _.template(IssueFilterTemplate),
 				
+				events: {
+					'click #filter': 'render',
+					'click #set-issue-filter': 'setFilter'
+				},			
+					
+
 				initialize: function() {
 					
 				},
 				
 				render: function(id) {
-					this.$el.html(that.template);
+					that = this;
+					categoryCollection.fetch({success: function(){
+							$('#form-container').html(that.template({"categories":that.categoryCollection.toJSON()}));
+							console.log(that.categoryCollection.toJSON());
+						}
+					});
 				},
 				
-				events: {
+				renderMarkers: function(){
+					//mapViev.model = 
+				},
 
-				},			
-			
-			return IssueDetaisView;
+				setFilter: function(){
+
+				}
+
+			});
+			return IssueFilterView;
 		})
