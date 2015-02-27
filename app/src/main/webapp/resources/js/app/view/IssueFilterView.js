25650989872575b9e8440456f657d224777a1e70
Nazar Kostecki <silver_925@ukr.net>
2015-02-28 00:11:23 +0200

    filter by category work correctly

--- app/src/main/webapp/resources/js/app/view/IssueFilterView.js
+++ app/src/main/webapp/resources/js/app/view/IssueFilterView.js
@@ -32,13 +32,14 @@ define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel', 'model/Category
 				},
 				
 				renderMarkers: function(){
+					mapView.cleanMap();
 					mapView.model = this.issueFiltredColection;
 					mapView.model.each(function(issue) {
 						L.marker(issue.get("mapPointer").substr(7, issue.get("mapPointer").length - 1)
-								.split(', '), { icon: mapView.markers[Math.floor(Math.random() * 5)] }).addTo(map).title = issue.get("id"); // delete from this line on('click', onMareker...). 
+								.split(', '), { icon: mapView.markers[Math.floor(Math.random() * 5)] }).addTo(map).on('click', onMarkerClick).title = issue.get("id"); // delete from this line on('click', onMareker...). 
 					});
 					//mapView.render();
-					console.log("issueFiltredColection" + this.issueFiltredColection.toJSON());
+					//console.log("issueFiltredColection" + this.issueFiltredColection.toJSON());
 					
 				},
 
@@ -58,5 +59,10 @@ define([ 'jquery', 'underscore', 'backbone', 'model/IssueModel', 'model/Category
 				}
 
 			});
+			function onMarkerClick(e) {
+				//issueDetailsView.render(this.title);
+				//TODO:replace comment list in issue details view
+				Backbone.history.navigate("issue/"+this.title,true)
+			}
 			return IssueFilterView;
 		})
