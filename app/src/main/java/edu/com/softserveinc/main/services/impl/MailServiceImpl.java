b6c93858e8a04d868324b69a7cc7d7c09b336904
Roman Vasylyshyn <roman@vasylyshyn.name>
2015-03-02 18:40:35 +0200

    Implemented notifyForIssue method in mail service.

--- app/src/main/java/edu/com/softserveinc/main/services/impl/MailServiceImpl.java
+++ app/src/main/java/edu/com/softserveinc/main/services/impl/MailServiceImpl.java
@@ -1,13 +1,17 @@
 package edu.com.softserveinc.main.services.impl;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import edu.com.softserveinc.main.models.SubscriptionModel;
 import edu.com.softserveinc.main.services.MailService;
 import edu.com.softserveinc.main.services.SubscriptionService;
 import edu.com.softserveinc.main.services.UserService;
@@ -21,6 +25,9 @@ public class MailServiceImpl implements MailService {
 	@Autowired private UserService userService;
 	
 	final String BAWL_EMAIL = "bawlrivne@gmail.com";
+	final String HEADER = "<html><body><h3>Bawl notification</h3><br /><p>";
+	final String BEFORE_LINK = "</p><br /><p>You may <a href=";
+	final String AFTER_LINK = ">unsubscribe</a> at any time.</p></body></html>";
 	
 	private MimeMessage composeUserMessage(String to, String msg)
 			throws MessagingException {
@@ -36,9 +43,34 @@ public class MailServiceImpl implements MailService {
 		return message;
 	}
 
+	private MimeMessage composeIssueMessage(SubscriptionModel sub, String msg)
+			throws MessagingException {
+
+		String digest = DigestUtils.md5DigestAsHex(sub.toString().getBytes());
+		String link = "http://localhost:8080/Bawl/" + sub.getId() + "/delete/" + digest;
+				
+		MimeMessage message = mailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(message, false);
+		helper.setFrom(BAWL_EMAIL);
+		helper.setSubject("Bawl notification");
+		helper.setTo(sub.getEmail());
+		helper.setText(HEADER+msg+BEFORE_LINK+link+AFTER_LINK, true);
+		
+		return message;
+	}
+	
 	@Override
 	public void notifyForIssue(int issueId, String msg) {
-
+		Collection<SubscriptionModel> subs = subService.listByIssueId(issueId);
+		for (SubscriptionModel sub: subs){
+			try {
+				MimeMessage composedMessage = composeIssueMessage(sub, msg);
+				mailSender.send(composedMessage);
+			} catch (MessagingException ex) {
+				System.out.println("Mail problem: " + ex.getCause());
+			}
+		}
 		
 	}
 
