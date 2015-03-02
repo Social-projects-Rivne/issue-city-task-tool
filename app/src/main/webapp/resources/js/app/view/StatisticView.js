e07582695ff3c8ad8f483a63f8bccf3dd8fc93f9
zorin <mykhailo.zorin@gmail.com>
2015-03-02 18:25:28 +0200

    Added methods to statistic view and added this view to main.js

--- app/src/main/webapp/resources/js/app/view/StatisticView.js
+++ app/src/main/webapp/resources/js/app/view/StatisticView.js
@@ -2,6 +2,56 @@ define([ 'jquery', 'underscore', 'backbone' ],
 		function($, _, Backbone) {
 			var StatisticView = Backbone.View.extend({
 				
+				initialize: function() {
+					console.log('Stat init');
+				},
+				
+				render: function() {
+					console.log('Stat render');
+				},
+				
+				statisticByCategories: function() {
+					$.ajax( {
+						type: 'POST',
+						url: 'statistic-by-category',
+						dataType: 'json',
+						success: function(data) {
+							console.log(data);
+						},
+						error: function() {
+							console.log('Error!');
+						}
+					} );
+				},
+				
+				statisticByStatuses: function() {
+					$.ajax( {
+						type: 'POST',
+						url: 'statistic-by-status',
+						dataType: 'json',
+						success: function(data) {
+							console.log(data);
+						},
+						error: function() {
+							console.log('Error!');
+						}
+					} );
+				},
+				
+				statisticByComments: function() {
+					$.ajax( {
+						type: 'POST',
+						url: 'statistic-by-comments',
+						dataType: 'json',
+						success: function(data) {
+							console.log(data);
+						},
+						error: function() {
+							console.log('Error!');
+						}
+					} );
+				}
+				
 			});	
 			
 			return StatisticView;
