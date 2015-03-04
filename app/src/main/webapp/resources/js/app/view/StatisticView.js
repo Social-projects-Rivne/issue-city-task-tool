d1fa87afa6582ab6019e06288eaf207eb77b53b6
Volodymyr Chmil <volodymyr.chmil@gmail.com>
2015-03-04 12:02:11 +0200

    Added d3js diagrams to StatisticView

--- app/src/main/webapp/resources/js/app/view/StatisticView.js
+++ app/src/main/webapp/resources/js/app/view/StatisticView.js
@@ -10,13 +10,48 @@ define([ 'jquery', 'underscore', 'backbone' ],
 					console.log('Stat render');
 				},
 				
+				statDiagram: function(data) {
+					var color = d3.scale.category10();
+					
+					var svg = d3.select("body").append("svg")
+						.attr("width", 1000)
+						.attr("height", 1000);
+					
+					var g = svg.append("g")
+						.attr("transform", "translate(300, 300)");
+					
+					var arc = d3.svg.arc()
+						.innerRadius(0)
+						.outerRadius(200);
+					
+					var pie = d3.layout.pie()
+						.value(function(d) { return d.value; });
+					
+					var arcs = g.selectAll(".arc")
+						.data(pie(data))
+						.enter()
+						.append("g")
+						.attr("class", "arc");
+					
+					arcs.append("path")
+						.attr("d", arc)
+						.attr("fill", function(d, i) { return color(i); });
+					
+					arcs.append("text")
+						.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+						.attr("text-anchor", "middle")
+						.text(function(d, i) { return data[i].value != "0"?data[i].label:""; });
+				},
+				
 				statisticByCategories: function() {
+					var that = this;
 					$.ajax( {
 						type: 'POST',
 						url: 'statistic-by-categories',
 						dataType: 'json',
 						success: function(data) {
 							console.log(data);
+							that.statDiagram(data);
 						},
 						error: function() {
 							console.log('Error!');
@@ -25,12 +60,14 @@ define([ 'jquery', 'underscore', 'backbone' ],
 				},
 				
 				statisticByStatuses: function() {
+					var that = this;
 					$.ajax( {
 						type: 'POST',
 						url: 'statistic-by-statuses',
 						dataType: 'json',
 						success: function(data) {
 							console.log(data);
+							that.statDiagram(data);
 						},
 						error: function() {
 							console.log('Error!');
@@ -39,12 +76,14 @@ define([ 'jquery', 'underscore', 'backbone' ],
 				},
 				
 				statisticByComments: function() {
+					var that = this;
 					$.ajax( {
 						type: 'POST',
 						url: 'statistic-by-comments',
 						dataType: 'json',
 						success: function(data) {
 							console.log(data);
+							that.statDiagram(data);
 						},
 						error: function() {
 							console.log('Error!');
