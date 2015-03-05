714f57acf504db9ae3b48e5bcee7573cb923113d
Roman Vasylyshyn <roman@vasylyshyn.name>
2015-03-06 01:12:16 +0200

    Cleaned HomeController. Removed explicit logging.

--- app/src/main/java/edu/com/softserveinc/main/controllers/HomeController.java
+++ app/src/main/java/edu/com/softserveinc/main/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package edu.com.softserveinc.main.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +9,6 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		Logger log = LoggerFactory.getLogger(HomeController.class);
-		log.debug("User go home!");
 		return "home";
 	}
 
