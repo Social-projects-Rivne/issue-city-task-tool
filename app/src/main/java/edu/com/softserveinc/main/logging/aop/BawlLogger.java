220f4465b476a95cf3a2f6be94cf5d38db6b2979
Roman Vasylyshyn <roman@vasylyshyn.name>
2015-03-05 19:01:43 +0200

    Added @Component to aspect class.

--- app/src/main/java/edu/com/softserveinc/main/logging/aop/BawlLogger.java
+++ app/src/main/java/edu/com/softserveinc/main/logging/aop/BawlLogger.java
@@ -4,8 +4,10 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class BawlLogger {
 
 	@Pointcut("execution(public * *(..))")
