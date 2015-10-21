0b7caea28664af9a3f426b8bdee1eefb2ee93249
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-10-21 21:12:42 +0300

    code cleanup

--- app/src/test/java/edu/com/softserveinc/bawl/services/CommentServiceTest.java
+++ app/src/test/java/edu/com/softserveinc/bawl/services/CommentServiceTest.java
@@ -1,7 +1,8 @@
-package edu.com.softserveinc.bawl.services.impl;
+package edu.com.softserveinc.bawl.services;
 
 import edu.com.softserveinc.bawl.dao.CommentDao;
 import edu.com.softserveinc.bawl.models.CommentModel;
+import edu.com.softserveinc.bawl.services.impl.CommentServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,31 +15,24 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Oleg on 20.10.2015.
- */
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:test-root-context.xml", "classpath:test-data-context.xml","classpath:test-mail-context.xml"} )
+@ContextConfiguration(locations = {"classpath:test-root-context.xml", "classpath:test-data-context.xml", "classpath:test-mail-context.xml"})
 
-public class CommentServiceImplTest extends Mockito {
+public class CommentServiceTest extends Mockito {
 
-
-
-    private CommentServiceImpl commentService ;
+    private CommentService commentService;
     private CommentDao commentDao;
 
 
     @Before
     public void setup() {
-    commentService = new CommentServiceImpl();
+        commentService = new CommentServiceImpl();
         commentDao = mock(CommentDao.class);
         Whitebox.setInternalState(commentService, "commentDao", commentDao);
     }
 
     @Test
     public void testAddComment() throws Exception {
-
         CommentModel mockCommentModel = mock(CommentModel.class);
         commentService.addComment(mockCommentModel);
         verify(commentDao, times(1)).saveAndFlush(mockCommentModel);
@@ -46,16 +40,13 @@ public class CommentServiceImplTest extends Mockito {
 
     @Test
     public void testDeleteComment() throws Exception {
-
         CommentModel mockCommentModel = mock(CommentModel.class);
         commentService.deleteComment(mockCommentModel);
         verify(commentDao, times(1)).delete(mockCommentModel);
-
     }
 
     @Test
     public void testEditComment() throws Exception {
-
         CommentModel mockCommentModel = mock(CommentModel.class);
         commentService.editComment(mockCommentModel);
         verify(commentDao, times(1)).saveAndFlush(mockCommentModel);
@@ -63,12 +54,10 @@ public class CommentServiceImplTest extends Mockito {
 
     @Test
     public void testGetCommentsByIssueId() throws Exception {
-
-            int IssueId = 2;
-            List<CommentModel> findComments = commentService.getCommentsByIssueId(IssueId);
-
-            for(CommentModel model : findComments){
-                assertEquals(IssueId, model.getIssueId() );
-            }
+        int IssueId = 2;
+        List<CommentModel> findComments = commentService.getCommentsByIssueId(IssueId);
+        for (CommentModel model : findComments) {
+            assertEquals(IssueId, model.getIssueId());
         }
     }
+}
--- app/src/test/scala/edu/com/softserveinc/bawl/controllers/CategoryControllerTest.scala
+++ app/src/test/scala/edu/com/softserveinc/bawl/controllers/CategoryControllerTest.scala
@@ -4,19 +4,22 @@ import java.util
 
 import edu.com.softserveinc.bawl.models.CategoryModel
 import edu.com.softserveinc.bawl.services.CategoryService
+import edu.com.softserveinc.bawl.services.impl.CategoryServiceImpl
 import org.junit.{Before, Test}
 import org.junit.runner.RunWith
 import org.mockito.{InjectMocks, Mock, MockitoAnnotations}
 import org.mockito.Mockito._
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.server.MockMvc
-import org.springframework.test.web.server.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.server.request.MockMvcRequestBuilders.{get,post}
 import org.springframework.test.web.server.result.MockMvcResultMatchers.{content, status}
 import org.springframework.test.web.server.setup.MockMvcBuilders
 
 @RunWith(classOf[SpringJUnit4ClassRunner])
-@ContextConfiguration(locations = Array("classpath:test-root-context.xml", "classpath:test-data-context.xml", "classpath:test-mail-context.xml", "classpath:test-root-context.xml")) class CategoryControllerTest {
+@ContextConfiguration(locations = Array("classpath:test-root-context.xml", "classpath:test-data-context.xml", "classpath:test-mail-context.xml", "classpath:test-root-context.xml"))
+class CategoryControllerTest {
 
   private var mockMvc: MockMvc = null
   @InjectMocks private var categoryController: CategoryController = null
@@ -39,6 +42,16 @@ import org.springframework.test.web.server.setup.MockMvcBuilders
     categories.add(model)
     when(categoryService.loadCategoriesList()).thenReturn(categories)
     mockMvc.perform(get("/categories")).andExpect(status.isOk).andExpect(content.string("[{\"id\":1,\"name\":\"foobar\"}]"))
+  }
+
+  @Test
+  @throws(classOf[Exception])
+  def testAddNewCategory {
+    val model: CategoryModel = new CategoryModel
+    model.setId(1)
+    model.setName("foobar")
+    mockMvc.perform(post("/categories/add").contentType(MediaType.APPLICATION_JSON).body("{\"id\":1,\"name\":\"foobar\"}".getBytes)).andExpect(status.isOk)
+
   }
 
 }
