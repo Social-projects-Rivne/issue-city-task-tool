dbe6005059c403a500c283eddb6f4104590bde0e
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-10-21 13:52:21 +0300

    cleanup

--- app/src/test/scala/edu/com/softserveinc/bawl/controllers/CategoryControllerTest.scala
+++ app/src/test/scala/edu/com/softserveinc/bawl/controllers/CategoryControllerTest.scala
@@ -2,21 +2,18 @@ package edu.com.softserveinc.bawl.controllers
 
 import java.util
 
-import edu.com.softserveinc.bawl.dao.CategoryDao
 import edu.com.softserveinc.bawl.models.CategoryModel
 import edu.com.softserveinc.bawl.services.CategoryService
-import org.hamcrest.Matchers
 import org.junit.{Before, Test}
 import org.junit.runner.RunWith
-import org.mockito.{Mockito, InjectMocks, Mock, MockitoAnnotations}
+import org.mockito.{InjectMocks, Mock, MockitoAnnotations}
+import org.mockito.Mockito._
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.server.MockMvc
-import org.springframework.test.web.server.request.MockMvcRequestBuilders.{delete, get, post, put}
-import org.springframework.test.web.server.result.MockMvcResultMatchers.{status, view, content}
+import org.springframework.test.web.server.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.server.result.MockMvcResultMatchers.{content, status}
 import org.springframework.test.web.server.setup.MockMvcBuilders
-import org.hamcrest.Matchers._;
-import org.mockito.Mockito._;
 
 @RunWith(classOf[SpringJUnit4ClassRunner])
 @ContextConfiguration(locations = Array("classpath:test-root-context.xml", "classpath:test-data-context.xml", "classpath:test-mail-context.xml", "classpath:test-root-context.xml")) class CategoryControllerTest {
