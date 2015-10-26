3f0959d0078f425a18e46df1aee3bbfc5edb1e7e
Illia Zolotariov <elvissrivne@gmail.com>
2015-10-26 17:16:05 +0200

    Integration and functional test for HistoryController

--- app/src/test/java/edu/com/softserveinc/bawl/controllers/HistoryControllerIntegrationTest.java
+++ app/src/test/java/edu/com/softserveinc/bawl/controllers/HistoryControllerIntegrationTest.java
@@ -1,52 +1,73 @@
 package edu.com.softserveinc.bawl.controllers;
 
-import edu.com.softserveinc.bawl.AbstractBawlTest;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.server.MockMvc;
 import org.springframework.test.web.server.MvcResult;
 import org.springframework.test.web.server.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.validation.constraints.AssertTrue;
+import java.util.Iterator;
 
-import static org.springframework.test.web.server.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.server.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 
-/**
- * Created by Illia on 10/26/2015.
- */
-public class HistoryControllerIntegrationTest extends AbstractBawlTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:test-integration-data-root-context.xml"})
+public class HistoryControllerIntegrationTest {
 
   private MockMvc mockMvc  = null;
   String MEDIA_TYPE = "application/json;charset=UTF-8";
   String EMPTY_COLLECTION = "[]";
+  int USER_ID = 1;
+  int ISSUE_ID = 1;
+
+  String [] FIELDS_USERHISTORY_DTO = {"username", "issueName", "date", "roleName", "statusId"};
 
   @Autowired
   private HistoryController historyController;
 
   @Before
   public void setup() {
-    mockMvc = MockMvcBuilders.standaloneSetup(this.historyController).build();
+    this.mockMvc = MockMvcBuilders.standaloneSetup(this.historyController).build();
   }
 
   @Test
   public void testGetUserHistoryAction() throws Exception {
-    mockMvc.perform(get("/issue/1/history"))
+    MvcResult mvcResult = mockMvc.perform(get( String.format ("/issue/%1$d/history", ISSUE_ID)))
       .andExpect(status().isOk())
       .andExpect(content().mimeType(MEDIA_TYPE))
-      .andExpect(content().string(EMPTY_COLLECTION));
+      .andReturn();
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode rootArray = mapper.readTree(mvcResult.getResponse().getContentAsString());
+
+    for(JsonNode issueNode : rootArray){
+      Iterator<String> iterator = issueNode.getFieldNames();
+      while (iterator.hasNext())
+      {
+        String fieldName = iterator.next();
+        assertThat(fieldName, isIn(FIELDS_USERHISTORY_DTO));
+      }
+    }
   }
 
-    @Test
-    public void testgetUserIssuesHistories() throws Exception {
-        mockMvc.perform(get("/user/1/history"))
-                .andExpect(status().isOk())
-                .andExpect(content().mimeType(MEDIA_TYPE))
-                .andExpect(content().string(EMPTY_COLLECTION));
+  @Test
+  public void testgetUserIssuesHistories() throws Exception {
+    mockMvc.perform(get( String.format ("/user/%1$d/history", USER_ID)))
+            .andExpect(status().isOk())
+            .andExpect(content().mimeType(MEDIA_TYPE));
+    //TODO check fields in result query
+    //method getUserIssuesHistories() has some problems
     }
+
 
 }
