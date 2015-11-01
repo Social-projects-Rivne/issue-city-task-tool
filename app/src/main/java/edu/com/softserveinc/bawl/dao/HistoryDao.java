85523a8dd88c6caaa9da546ebf0c30c008ce1623
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-11-01 15:38:46 +0200

    tests fixed

--- app/src/main/java/edu/com/softserveinc/bawl/dao/HistoryDao.java
+++ app/src/main/java/edu/com/softserveinc/bawl/dao/HistoryDao.java
@@ -1,8 +1,6 @@
 package edu.com.softserveinc.bawl.dao;
 
 import edu.com.softserveinc.bawl.models.HistoryModel;
-import edu.com.softserveinc.bawl.models.IssueModel;
-import edu.com.softserveinc.bawl.query.HistoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,14 +12,29 @@ import java.util.List;
  */
 public interface HistoryDao  extends JpaRepository<HistoryModel, Integer> {
 
+    String uniqueLastByDateHistories = "SELECT h.* FROM history AS h, ( " +
+                    "    SELECT issue_id, MAX(id) AS ID " +
+                    "    FROM history " +
+                    "    GROUP BY issue_id " +
+                    ") AS hMaxId " +
+                    "WHERE hMaxId.id = h.id";
+
+    String lastIssueByIssueID = "SELECT h.* FROM history AS h , ( " +
+                    "    SELECT MAX(id) AS ID " +
+                    "    FROM history as h2 " +
+                    "    WHERE issue_id = :issueId " +
+                    "    GROUP BY issue_id " +
+                    ") AS hMaxId " +
+                    "WHERE h.id = hMaxId.ID";
+
     List <HistoryModel> findByUserId (int userId);
 
     List <HistoryModel> findByIssueId (int issueId);
 
-    @Query(value = HistoryQuery.uniqueLastByDateHistories, nativeQuery = true)
+    @Query(value = uniqueLastByDateHistories, nativeQuery = true)
     List <HistoryModel> getUniqueLastByDateHistories();
 
-    @Query(value = HistoryQuery.lastIssueByIssueID, nativeQuery = true)
+    @Query(value = lastIssueByIssueID, nativeQuery = true)
     HistoryModel getLastByIssueIDHistory(@Param("issueId") int issueId);
 
 
