27d1398dd48bf2c940629bbb41f0d0dfcafee89c
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-11-06 22:05:31 +0200

    code cleaning

--- app/src/main/scala/edu/com/softserveinc/bawl/dto/ResponseDTO.scala
+++ app/src/main/scala/edu/com/softserveinc/bawl/dto/ResponseDTO.scala
@@ -4,4 +4,6 @@ import scala.beans.BeanProperty
 
 class ResponseDTO {
   @BeanProperty  var message: String = null
+  @BeanProperty  var label: String = null
+  @BeanProperty  var value: String = null
 }
