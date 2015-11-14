e208282822b0aefadbe1571db504d5a0d6074f49
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-11-15 01:56:49 +0200

    code cleaning

--- app/src/main/java/edu/com/softserveinc/bawl/security/AjaxAuthenticationEntryPoint.java
+++ app/src/main/java/edu/com/softserveinc/bawl/security/AjaxAuthenticationEntryPoint.java
@@ -1,23 +1,23 @@
 package edu.com.softserveinc.bawl.security;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.apache.log4j.Logger;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import org.apache.log4j.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class AjaxAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    public static final Logger LOG=Logger.getLogger(AjaxAuthenticationEntryPoint.class);
+    public static final Logger LOG = Logger.getLogger(AjaxAuthenticationEntryPoint.class);
 
-	public void commence(HttpServletRequest request,
-			HttpServletResponse response, AuthenticationException authException)
-			throws IOException, ServletException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-	}
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
