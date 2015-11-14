a234dd0de36fdb5174e37344409d84b8bbdac3b9
Andriy <andriybalashov@MacBook-Pro-2.local>
2015-11-14 23:21:35 +0200

    code cleaning

--- app/src/main/scala/edu/com/softserveinc/bawl/dto/UserHistoryIssuesForUserDto.scala
+++
@@ -1,9 +0,0 @@
-package edu.com.softserveinc.bawl.dto
-
-import scala.beans.BeanProperty
-
-class UserHistoryIssuesForUserDto {
-  @BeanProperty var nameIssue: String = null
-  @BeanProperty var history: List[IssueHistoryDTO] = null
-
-}
--- app/src/main/scala/edu/com/softserveinc/bawl/dto/UserIssuesHistoryDto.scala
+++
@@ -1,9 +0,0 @@
-package edu.com.softserveinc.bawl.dto
-
-import scala.beans.BeanProperty
-
-class UserIssuesHistoryDTO {
-  @BeanProperty  var issueName: String = null
-  @BeanProperty  var issueHistoryDto: IssueHistoryDTO = null
-  @BeanProperty  var currentStatus: String = ""
-}
