a042a60a084d1f440221709b86ebd31f3e054e86
Vasyl-Paslavskyj <vasylpaslavskyj@gmail.com>
2015-11-16 18:27:38 +0200

    fix Manager view

--- app/src/main/webapp/resources/js/app/views/ManagerView.js
+++ app/src/main/webapp/resources/js/app/views/ManagerView.js
@@ -2,9 +2,9 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 		function($, bootstrap, _, Backbone, IssueCollection, ManagerTemplate, IssueTableTemplate, ManagerSearchTemplate, CategoryCollection, IssueModel, StatusCollection, NotificationTemplate, CategoryModel, ConfirmationTemplate, EditIssueTemplate) {
 			
 			var that = null;
-	
+
 			var ManagerView = Backbone.View.extend({
-				
+
 				events: {
 					'click #issue-filter  #filter-issue': 'issueFilter',
 					'click #issue-filter  #reset-filter-issue': 'resetFilter',
@@ -19,9 +19,11 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					'click .edit-issue'	: 'showEditIssueForm',
 					'click .editIssueConfirm' : 'editIssue',
 					'click #left_admin_panel #manager_log_out':'logOut',
-					'click #all_issues': 'allIssues'
+					'click #all_issues': 'allIssues',
+					'click #new_issues': 'newIssues',// New
+					'click #resolved_issues': 'resolvedIssues'// New
 				},
-				
+
 				managerTemplate: _.template(ManagerTemplate),
 				issueTableTemplate: _.template(IssueTableTemplate),
 				searchTemplate: _.template(ManagerSearchTemplate),
@@ -35,7 +37,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 				categories: null,
 				statuses: null,
 				issue: null,
-				
+
 				initialize: function() {
 					that = this;
 					this.issues = mapView.model;
@@ -47,7 +49,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					this.issue = new IssueModel();
 					this.categoriesCollection = issueFilterView.categoryCollection;
 				},
-				
+
 				// issue table on manager page
 				issueTableRender: function() {
 					this.$("#issue-table-body").empty();
@@ -65,12 +67,12 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 				searchRender: function(){
  					console.log('search rendered');
  					this.categoriesCollection.fetch({success: function(){
-						that.$('#issue-filter').append(that.searchTemplate({"categories":that.categoriesCollection.toJSON()})); 
+						that.$('#issue-filter').append(that.searchTemplate({"categories":that.categoriesCollection.toJSON()}));
 						}
 					});
 				},
-				
-				// render all components of manager page 
+
+				// render all components of manager page
 				render: function() {
 					that = this;
 					setTimeout(function(){
@@ -82,40 +84,40 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					}, 200)
 
 				},
-				
+
 				//for issue table style
 				issueFocus: function(e){
 					var categoryCell = $(e.currentTarget).find('#category-cell');
 					var statusCell = $(e.currentTarget).find('#status-cell');
-					
+
 					//hide div with category name
 					$($(categoryCell).find('#name')).hide();
-					//hide div with category status 
+					//hide div with category status
 					$($(statusCell).find('#name')).hide();
 
 					//show buttons
 					$(e.currentTarget.getElementsByClassName('btn-toolbar')[0]).fadeIn(50);
-					
+
 					//show all dropdun lists in row
 					$(e.currentTarget).find('select').show();
 					e.currentTarget.style.setProperty('background',"white");
 					//e.currentTarget.style.setProperty("font-weight","bold");
-				},	
+				},
 
 				//for issue table style
 				issueUnFocus: function(e){
-					
+
 					var categoryCell = $(e.currentTarget).find('#category-cell');
 					var statusCell = $(e.currentTarget).find('#status-cell');
-					
+
 					//hide buttons
 					$(e.currentTarget.getElementsByClassName('btn-toolbar')[0]).fadeOut(0);
-					
+
 					//change category cell
 					$($(categoryCell).find('#name')).empty();
 					$($(categoryCell).find('#name')).append($(categoryCell).find('select').val());
 					$($(categoryCell).find('#name')).show();
-					
+
 					//change status cell
 					$($(statusCell).find('#name')).empty();
 					$($(statusCell).find('#name')).append($(statusCell).find('select').val());
@@ -123,7 +125,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 
 					//hide all dropdun lists in row
 					$(e.currentTarget).find('select').hide();
-					
+
 					//change style of row
 					e.currentTarget.style.setProperty('background',"");
 					//e.currentTarget.style.setProperty("font-weight","");
@@ -150,9 +152,9 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					console.log("Status is " + $('#issue-filter #status').prop("checked") + ' id = ' + $('#issue-filter #status-filter').val());
 					console.log("Category is " + $('#issue-filter #category').prop("checked"));
 					console.log("Priority is " + $('#issue-filter #priority').prop("checked") + ' id = ' + $('#issue-filter #priority-filter').val());
-					
+
 					//filter by status (it work when raido btn Status checked)
-					if ($('#issue-filter #status').prop("checked")) {
+					if ($('#issue-filter #status').prop("checked")) {//!!!
 						var issuesFilterList = new IssueCollection();
 						this.issues.each(function(issue){
 							if(issue.get('status') == $('#issue-filter #status-filter').val()){
@@ -165,21 +167,21 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						console.log(this.issues);
 						this.issueTableRender();
 					};
-					
+
 					//search by name (it work when raido btn Name checked)
 					if( $('#issue-filter #name').prop("checked")) {
 						console.log("where Name is = " + $('#issue-filter #text-value-issue-filter').val());
 						var issuesFilterList = new IssueCollection();
 						this.issues.each(function(issue){
-							if(issue.get('name') == $('#issue-filter #text-value-issue-filter').val()){ 
+							if(issue.get('name') == $('#issue-filter #text-value-issue-filter').val()){
 								console.log(issue);
 								issuesFilterList.add(issue);
 							}
-						});		
+						});
 						console.log(issuesFilterList);
 						this.issues = issuesFilterList;
 						console.log(this.issues);
-						this.issueTableRender();			
+						this.issueTableRender();
 					};
 
 					//search by keyword (it work when raido btn Keyword checked)
@@ -188,15 +190,15 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						var issuesFilterList = new IssueCollection();
 						this.issues.each(function(issue){
 							if((issue.get('name').match($('#issue-filter #text-value-issue-filter').val()) != null) ||
-							 (issue.get('description').match($('#issue-filter #text-value-issue-filter').val()) != null)){ 
+							 (issue.get('description').match($('#issue-filter #text-value-issue-filter').val()) != null)){
 								console.log(issue);
 								issuesFilterList.add(issue);
 							}
-						});		
+						});
 						console.log(issuesFilterList);
 						this.issues = issuesFilterList;
 						console.log(this.issues);
-						this.issueTableRender();			
+						this.issueTableRender();
 					};
 
 					//filter by category (it work when raido btn Cetegory checked)
@@ -204,7 +206,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						var issuesFilterList = new IssueCollection();
 						// it must be filtred with findWhere
 						this.issues.each(function(issue){
-							if(issue.get('categoryId') == $('#issue-filter #categories').val()){ 
+							if(issue.get('categoryId') == $('#issue-filter #categories').val()){
 								console.log(issue);
 								issuesFilterList.add(issue);
 							}
@@ -219,7 +221,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					if ($('#issue-filter #priority').prop("checked")) {
 						var issuesFilterList = new IssueCollection();
 						this.issues.each(function(issue){
-							if(issue.get('priorityId') == $('#issue-filter #priority-filter').val()){ 
+							if(issue.get('priorityId') == $('#issue-filter #priority-filter').val()){
 								console.log(issue);
 								issuesFilterList.add(issue);
 							}
@@ -230,7 +232,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						this.issueTableRender();
 					};
 				},
-				
+
 				//reset filter
 				resetFilter: function(){
 					var that = this;
@@ -239,13 +241,13 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						}
 					});
 				},
-				
+
 				quickChangeCategory: function(e) {
 					this.issue = this.issues.get( e.currentTarget.id);
 					this.issue.set( {category: e.currentTarget.value});
 					this.issue.save();
 				},
-				
+
 				quickChangeStatus: function(e) {
 					this.issue = this.issues.get( e.currentTarget.id);
 					this.issue.set( {status: e.currentTarget.value});
@@ -281,42 +283,81 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					this.issue.save();
 					this.render();
 				},
-				
-
-
 
 				allIssues: function (e) {
-					this.$("#issue-table-body").empty();
-					that = this;
-					this.issues.each( function(issue){
-						that.$("#issue-table-body").append(that.$("#issue-table-body").
-								append(that.issueTableTemplate({data: [ {issue: issue.toJSON()}, {categories: that.categories.toJSON()}, {statuses: that.statuses.toJSON()} ] }))
-						);
+					this.issues = mapView.model;
+					var issuesFilterList = new IssueCollection();
+					this.issues.each(function(issue){
+						if(issue.get('status') == "NEW" || issue.get('status') == "APPROVED" || issue.get('status') == "TO_RESOLVE"){
+							console.log(issue);
+							issuesFilterList.add(issue);
+						}
 					});
-					this.$("#issue-table-body").find('select').hide();
+					console.log(issuesFilterList);
+					this.issues = issuesFilterList;
+					console.log(this.issues);
+					this.issueTableRender();
+					//this.$("#issue-table-body").empty();
+					//that = this;
+					//this.issues.each( function(issue){
+					//	that.$("#issue-table-body").append(that.$("#issue-table-body").
+					//			append(that.issueTableTemplate({data: [ {issue: issue.toJSON()}, {categories: that.categories.toJSON()}, {statuses: that.statuses.toJSON()} ] }))
+					//	);
+					//});
+					//this.$("#issue-table-body").find('select').hide();
 
+				},
+
+				newIssues: function (e) {
+					this.issues = mapView.model;
+					var issuesFilterList = new IssueCollection();
+					this.issues.each(function (issue) {
+						if (issue.get('status') == "NEW") {
+							console.log(issue);
+							issuesFilterList.add(issue);
+						}
+					});
+					console.log(issuesFilterList);
+					this.issues = issuesFilterList;
+					console.log(this.issues);
+					this.issueTableRender();
+				},
+
+				resolvedIssues: function (e) {//Dosen't worck!!!
+					this.issues = mapView.model;
+					var issuesFilterList = new IssueCollection();
+					this.issues.each(function (issue) {
+						if (issue.get('status') == "RESOLVED") {
+							console.log(issue);
+							issuesFilterList.add(issue);
+						}
+					});
+					console.log(issuesFilterList);
+					this.issues = issuesFilterList;
+					console.log(this.issues);
+					this.issueTableRender();
 				},
 
 				showEditIssueForm: function(e){
 					// remove existing modal
-					if($('#editIssueModal')) $('#editIssueModal').remove(); 
-					
+					if($('#editIssueModal')) $('#editIssueModal').remove();
+
 					//get issue from collection by ID for load fields in template
 					issue= this.issues.get(e.currentTarget.id);//+
 					console.log (issue.toJSON()); //+
 
 					this.$el.append(this.editIssueTemplate({data: [ {issue: issue.toJSON()}, {categories: that.categories.toJSON()}, {statuses: that.statuses.toJSON()} ] }));
-					
+
 					$('#editIssueModal').modal();
 					console.log ('--- --- data inserted from DB to fields ok'); //+
-					
+
 					// assign jQuery selectors for variables for will use for validation below
 					issueDescription = $('#edit-issue-form-description');
 					issueAttachment = $('#edit-issue-form-attachments');
 					issueCategory =	$('#edit-issue-form-category');
 					issueStatus = $('#edit-issue-form-status');
 					issuePriority =	$('#edit-issue-form-priority');
-					 
+
 					// RegExp validate for fields
 					issueDescription.on('blur', function() {
 						if (!/^[A-Za-z0-9]+[A-Za-z0-9\s]+[A-Za-z0-9]+$/.test(this.value)) {
@@ -360,7 +401,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						if (this.value == 'Wrong name!') this.value ='';
 						this.style.color = 'black';
 					});
-					
+
 				},
 
 				logOut: function(){
@@ -373,7 +414,7 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 					$('#notificationModal').modal();
 					loginView.buttonsManage();
 				},
-				
+
 				editIssue: function(e) {
 					var isValid = true;
 					if (!/^[A-Za-z0-9]+[A-Za-z0-9\s]+[A-Za-z0-9]+$/.test(issueDescription.val())) {
@@ -393,15 +434,15 @@ define([ 'jquery', 'bootstrap', 'underscore', 'backbone', 'collection/IssueColle
 						issuePriority.val('Wrong value!').css('color', 'red');
 						isValid = false;
 					}*/
-	
+
 					if(isValid) {
-						//call confirmation for edit issue				
+						//call confirmation for edit issue
 						if($('#confirmationModal')) $('#confirmationModal').remove();
 						this.$el.append(this.confirmationTemplate( { 'data': [ { 'message': 'Do you really want to edit this issue?' }, { 'id': e.currentTarget.id }, { 'action': 'edit issue' } ] } ));
 						$('#confirmationModal').modal();
 					}
 				}
-									
+
 			});
 			
 			return ManagerView;
